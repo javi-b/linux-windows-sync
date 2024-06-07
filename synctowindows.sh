6009fd08f039115f0b482a43f603019ec9c9d48d
javi-b <javibonafonte@gmail.com>
2024-06-07 20:50:04 +0200

    added bash script to sync from linux to windows

---
+++ synctowindows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo !!!this script removes previous files in target device before copying
+read -p "are you sure? (y/n): " answer
+
+if [[ $answer != "y" ]]
+then
+    exit
+fi
+
+echo removing previous files in target device...
+ssh Javi@Ivy-Bridge 'rd /s /q "C:\\Users\\Javi\\Pictures\\_synced"'
+echo copying files to target device...
+scp -r $HOME/pics/_synced Javi@Ivy-Bridge:C:/Users/Javi/Pictures/
+
