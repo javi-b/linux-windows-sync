30eb4a5df22b9b86d141381453cd37a0efeb2075
javi-b <javibonafonte@gmail.com>
2024-06-07 20:47:05 +0200

    added batch script to sync from windows to linux

---
+++ synctolinux.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo !!!this script removes previous files in target device before copying
+set /p answer="are you sure? (y/n): "
+
+if /I "%answer%" NEQ "y" (
+    exit /b
+)
+
+echo removing previous files in target device...
+ssh shinji@wode "rm -rf $HOME/pics/_synced"
+echo copying files to target device...
+scp -r %USERPROFILE%\Pictures\_synced shinji@wode:/home/shinji/pics/
